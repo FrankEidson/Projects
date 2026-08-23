edd4f81cdf9f8e0f6a3d7f8657a08997959f72d6
FrankEidson <eidson_donald@students.columbusstate.edu>
2026-08-23 16:14:47 -0400

    Add files via upload

---
+++ first.java
@@ -0,0 +1,8 @@
+public class first {
+    //Frank Eidson
+    static void main(String[] args)
+    {System.out.print("Hello everyone! \n" +
+            "Job Title: Network Admin\n" +
+            "Why: Being able to protect information primary to my job \n" +
+            "Median Income: $176.000 annual\n" );}
+}
