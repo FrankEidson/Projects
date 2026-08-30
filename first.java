d1cc82a4bc83c25f92004808865680d7a7770688
FrankEidson <eidson_donald@students.columbusstate.edu>
2026-08-30 03:08:32 -0400

    Change greeting from 'Hello everyone!' to 'Hola Mundo!'

--- first.java
+++ first.java
@@ -1,7 +1,7 @@
 public class first {
     //Frank Eidson
     static void main(String[] args)
-    {System.out.print("Hello everyone! \n" +
+    {System.out.print("Hola Mundo! \n" +
             "Job Title: Network Admin\n" +
             "Why: Being able to protect information primary to my job \n" +
             "Median Income: $176.000 annual\n" );}
